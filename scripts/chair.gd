2fc40662a36f6cb99b7e578cc1d9bbedb6cb9ac1
Rémi Morvan <remi@morvan.xyz>
2025-04-26 20:46:07 +0200

    glowing butterfly

--- scripts/chair.gd
+++ scripts/chair.gd
@@ -1,7 +1,7 @@
 extends GameObject
 
 @onready var sprite = $"CharacterBody2D/Sprite2D"
-#@onready var shader = sprite.get_material() 
+@onready var shader = sprite.get_material() 
 
 var init_scale: Vector2
 
@@ -10,10 +10,12 @@ func _ready() -> void:
 	init_scale = sprite.scale
 
 func highlight() -> void:
-	sprite.scale = init_scale*1.4
+	#sprite.scale = init_scale*1.4
+	shader.set_shader_parameter("clr", Vector4(1.0, 0.9, 0.2, 1.0))
 	
 func unhighlight() -> void:
-	sprite.scale = init_scale
+	#sprite.scale = init_scale
+	shader.set_shader_parameter("clr", Vector4(1.0, 0.9, 0.2, 0.0))
 
 func interact() -> void:
 	pass
