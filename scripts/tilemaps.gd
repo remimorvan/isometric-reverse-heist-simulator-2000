50607ae2ca5c7fc235aab63c766bbd270e97c088
Rémi Morvan <remi@morvan.xyz>
2025-04-26 20:09:39 +0200

    clean tilemaps

--- scripts/tilemaps.gd
+++ scripts/tilemaps.gd
@@ -5,7 +5,6 @@ extends Node2D
 @onready var chair: TileMapLayer = $Chair
 
 var hover_effect: Polygon2D
-var select_effects: Dictionary
 
 func create_glow(color: Color, layer_index: int) -> Polygon2D:
 	var glow = Polygon2D.new()
@@ -23,30 +22,13 @@ func create_glow(color: Color, layer_index: int) -> Polygon2D:
 func make_hover_glow(position: Vector2i) -> void:
 	hover_effect.position = layer0.map_to_local(position)
 	hover_effect.visible = true
-	
-func make_select_glow(tile_position: Vector2i) -> void:
-	var glow = select_effects[tile_position]
-	glow.position = layer1.map_to_local(tile_position)
-	glow.position[1] -= 16
-	glow.visible = true
 
 func stop_hover_glow() -> void:
 	hover_effect.visible = false
-	
-func stop_select_glow(tile_position: Vector2i) -> void:
-	select_effects[tile_position].visible = false
 
 func _ready() -> void:
 	hover_effect = create_glow(Color(1, 1, 1, 0.2), 0)
 	$Layer0.add_child(hover_effect)
-	# We create a glow for every cell **at layer 0**, however this
-	# glow in on **layer 1**.
-	# We implicitely assume that for every cell at layer 1, there is
-	# a corresponding cell at layer 0.
-	var cells = layer0.get_used_cells()
-	for c in cells:
-		select_effects[c] = create_glow(Color(1, 0.9, 0.3, 0.2), 1)
-		$Layer1.add_child(select_effects[c])	
 
 func _process(_delta: float) -> void:
 	var mouse_pos = get_local_mouse_position()
