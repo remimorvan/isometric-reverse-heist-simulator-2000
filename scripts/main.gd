ca4905fc141141483b7cee35fdce12fa3eea97d8
Rémi Morvan <remi@morvan.xyz>
2025-04-27 22:35:35 +0200

    restrart button

--- scripts/main.gd
+++ scripts/main.gd
@@ -9,3 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_restart_button_pressed() -> void:
+	get_tree().reload_current_scene()
