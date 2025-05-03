1410b462d7f07583e9e04291c8ab13c894a0b51d
Rémi Morvan <remi@morvan.xyz>
2025-05-03 22:10:59 +0200

    background

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
+@onready var background: Sprite2D = $Background
+var initial_position: Vector2
+var initial_y_position: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	initial_position = background.global_position
+	initial_y_position = background.global_position[1]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	background.global_position += Vector2(0,200)*delta
+	if background.global_position[1] >= initial_y_position + 1080 * background.scale[1]:
+		background.global_position = initial_position
 
 
 func _on_restart_button_pressed() -> void:
